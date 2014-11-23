9deda84274a184a63c4191f1b0733f9af631db6a
les87 <lesliekifuse@me.com>
2014-11-23 00:05:48 +0000

    Update

--- proto/views.py
+++ proto/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from proto.models import Printer
 
-# Create your views here.
+def printers(request):
+	return render_to_response('printers.html',
+							{'printers': Printer.objects.all()})
+							
+def printer(request, printer_id=1):
+	return render_to_response('printer.html',
+							{'printer' : Printer.objects.get(id=printer_id)})
+					
