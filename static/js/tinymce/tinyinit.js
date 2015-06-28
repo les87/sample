7167cb40613c0b4937dc59c6139a379fc2bea9aa
les87 <lesliekifuse@me.com>
2015-06-28 15:59:07 +0100

    Working fine

--- static/js/tinymce/tinyinit.js
+++ static/js/tinymce/tinyinit.js
@@ -24,13 +24,14 @@ plugins: [
 // PUT PLUGIN'S BUTTON on the toolbar
 // ===========================================
 	
-toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image preview",
 	
 // ===========================================
 // SET RELATIVE_URLS to FALSE (This is required for images to display properly)
 // ===========================================
 	
 relative_urls: false
+
 	
 });
 
